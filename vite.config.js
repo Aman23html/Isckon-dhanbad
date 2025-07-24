cc008d309c0a71ea45e015ead08b9c637f759a93
Aman23html <aman.atka2345@gmail.com>
2025-07-24 10:54:44 +0530

    Some update

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/Isckon-dhanbad', // Adjust this if your app is hosted in a subdirectory
+  build: {
+    outDir: 'dist',
+  },
 })
